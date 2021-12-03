8c064075a128a03a01f28d0f0e5991c8b4827621
Denis Bezrukov <anthrax63@gmail.com>
2021-12-03 10:11:50 +0300

    fix

--- Lesson3/setBlocksHomeWork.py
+++ Lesson3/setBlocksHomeWork.py
@@ -5,18 +5,17 @@ import mcpi.block as block
 
 mc = Minecraft.create()
 
-mc.player.setPos(random.randrange(0, 1000), random.randrange(80, 180), random.randrange(0, 1000))
+mc.player.setPos(random.randrange(0, 100), random.randrange(100, 180), random.randrange(0, 100))
 pos = mc.player.getPos()
 
 x = pos.x
 y = pos.y
 z = pos.z
+platformY = y - 5
 
-mc.setBlocks(x - 1, y, z - 1, x + 1, y + 1, z + 1, block.GRASS)
-mc.setBlocks(x - 1, y + 1, z - 1, x + 1, y + 2, z + 1, block.FLOWER_YELLOW)
+mc.setBlocks(x - 1, platformY, z - 1, x + 1, platformY + 1, z + 1, block.GRASS)
+mc.setBlocks(x - 1, platformY + 1, z - 1, x + 1, platformY + 2, z + 1, block.FLOWER_YELLOW)
 
-mc.setBlock(x, y + 1, z, block.AIR)
+mc.setBlock(x, platformY + 1, z, block.AIR)
 
-
-mc.player.setPos(x + 2, y + 2, z + 2)
 
