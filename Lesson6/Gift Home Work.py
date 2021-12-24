08c4d88915611f8a2ca565caeb06ced66747d0be
Denis Bezrukov <anthrax63@gmail.com>
2021-12-24 21:56:57 +0300

    Переделал lp

--- Lesson6/Gift Home Work.py
+++ Lesson6/Gift Home Work.py
@@ -1,11 +1,26 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
+import time
+
 
 
 x = 172
 y = 67
 z = 302
 
+
+mc.postToChat("Поставьте блок на королевский пьедистал!(У тебе есть пять секунд время пошло!)")
+
+mc.postToChat("5")
+time.sleep(1)
+mc.postToChat("4")
+time.sleep(1)
+mc.postToChat("3")
+time.sleep(1)
+mc.postToChat("2")
+time.sleep(1)
+mc.postToChat("1 Началось Надеюсь положил подарок иначе зарежу!")
+time.sleep(1)
 
 # Узнаём id блока в отсеке для подарков
 block = mc.getBlock(x, y, z)
--- SelfWork/Task2.py
+++ SelfWork/Task2.py
@@ -3,7 +3,6 @@ import random
 import mcpi.block as block
 
 mc = Minecraft.create()
-mc.player.setTilePos(random.randrange(0, 100), random.randrange(100, 180), random.randrange(0, 100))
 
 pos = mc.player.getPos()
 
@@ -23,3 +22,20 @@ mc.setBlocks(
     x + 10, y + 9, z + 9,  # координата 2-й точки
     block.AIR
 )
+answer = input("Вы хотите удалить дом ? да/нет :")
+if answer == "да":
+    mc.setBlocks(x, y, z, x + 10, y + 10, z + 10,         block.AIR
+)
+    mc.setBlocks(
+        x + 1, y + 1, z,  # координата 1-й точки
+        x + 9, y + 9, z + 10,  # координата 2-й точки
+        block.AIR
+    )
+    mc.setBlocks(
+        x, y + 1, z + 1,  # координата 1-й точки
+           x + 10, y + 9, z + 9,  # координата 2-й точки
+        block.AIR
+    )
+else :
+    mc.postToChat(" Хорошо !Спасибо что пользуетесь услугами построй дом!")
+    print(" Хорошо !Спасибо что пользуетесь услугами построй дом!")
