2e2255577aef78fc9fbe90f5d395b0d5cc7e13b0
Denis Bezrukov <anthrax63@gmail.com>
2022-01-08 20:01:13 +0300

    дз

--- Lesson8/planting_forest.py
+++ Lesson8/planting_forest.py
@@ -6,8 +6,8 @@ mc = Minecraft.create()
 def growTree(x, y, z):
     mc.setBlocks(x, y, z, x + 1, y + 1, z + 1, 2)
     mc.setBlocks(x, y, z, x, y + 7, z, 17)
-    mc.setBlocks(x - 3, y + 8, z - 3, x + 3, y + 8, z + 3, 161)
-    mc.setBlocks(x - 2, y + 9, z - 2, x + 2, y + 9, z + 2, 161)
+    mc.setBlocks(x - 3, y + 8, z - 3, x + 3, y + 8, z + 3, 161.1 )
+    mc.setBlocks(x - 2, y + 9, z - 2, x + 2, y + 9, z + 2, 161.1)
 
 
 pos = mc.player.getTilePos()
